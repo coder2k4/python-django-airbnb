341f9a83d1c149ae7939cabec8fd462f428509b1
coder2k@mail.ru <standalone!@##@!!@#>
2020-04-08 18:36:25 +0300

    Начата кастомизация отображения административной части

--- users/admin.py
+++ users/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from users.models import User
 
-admin.site.register(User)
+
+class AuthorAdmin(admin.ModelAdmin):
+    """ Custom user admin """
+
+    list_display = ('username', 'gender', 'language', 'currency', 'superhost')
+    list_filter = ('gender',)
+
+
+admin.site.register(User, AuthorAdmin)
