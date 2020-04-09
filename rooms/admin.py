7efa02f97b5a2ab783fcbcee060e61eaae9b7aa7
coder2k@mail.ru <standalone!@##@!!@#>
2020-04-09 12:57:04 +0300

    расширена модель комнат

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
-from rooms.models import Room
+from rooms.models import Room, RoomType, Amenity, Facility, HouseRule, Photo
+
+
+@admin.register(RoomType, Amenity, Facility, HouseRule)
+class ItemAdmin(admin.ModelAdmin):
+    """ Item admin """
+    pass
 
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+    """ Room admin """
+    pass
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """ Photo admin """
     pass
--- rooms/models.py
+++ rooms/models.py
@@ -4,6 +4,52 @@ from users.models import User
 from core import models as core_models
 
 
+class AbstractItem(core_models.TimeStampedModel):
+    """ Абстрактный класс, добавляюшщий поле name """
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+    """ RoomType """
+    ordering = ['-created']
+    pass
+
+
+class Amenity(AbstractItem):
+    """ Amenity """
+    pass
+
+    class Meta:
+        verbose_name = "Amenity"
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+    """ Facility """
+    pass
+
+
+class HouseRule(AbstractItem):
+    """ HouseRule """
+    pass
+
+
+class Photo(AbstractItem):
+    """ Photo """
+    caption = models.CharField(max_length=90)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
+
+
 class Room(core_models.TimeStampedModel):
     """ Определение модели ROOM, наследуемся от  TimeStampedModel"""
 
@@ -21,3 +67,10 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(User, on_delete=models.CASCADE)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenity = models.ManyToManyField(Amenity, blank=True)
+    facility = models.ManyToManyField(Facility, blank=True)
+    house_rules = models.ManyToManyField(HouseRule, blank=True)
+
+    def __str__(self):
+        return self.name
