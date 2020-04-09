b612c52e70bd524c1799b311310bc461fd75cb2b
coder2k@mail.ru <standalone!@##@!!@#>
2020-04-09 09:44:23 +0300

    USERS - сделана кастомная админка.

--- users/admin.py
+++ users/admin.py
@@ -1,14 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
 from users.models import User
 
 
-class AuthorAdmin(admin.ModelAdmin):
+class AuthorAdmin(UserAdmin):
     """ Custom user admin """
+    fieldsets = UserAdmin.fieldsets + (
+        ("Custom Profile", {
+             "fields": ("avatar",
+                        "gender",
+                        "bio",
+                        "birthday",
+                        "language",
+                        "currency",
+                        "superhost",
+                        )
+        }),
+    )
 
-    list_display = ('username', 'gender', 'language', 'currency', 'superhost')
-    list_filter = ('gender',)
+    # list_display = ('username', 'gender', 'language', 'currency', 'superhost')
+    # list_filter = ('gender',)
 
 
 admin.site.register(User, AuthorAdmin)
